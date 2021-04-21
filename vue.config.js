ed6d871cb417ad99a2d83c8d0b634df293697ebf
haryan248 <haryan96@naver.com>
2021-04-21 23:40:50 +0900

    api 서버 제거

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
 	devServer: {
 		compress: true,
-		proxy: {
-			"^/api": {
-				target: "http://101.101.218.36:8000",
-				changeOrigin: true,
-				logLevel: "debug",
-			},
-		},
+		// proxy: {
+		// 	"^/api": {
+		// 		target: "http://101.101.218.36:8000",
+		// 		changeOrigin: true,
+		// 		logLevel: "debug",
+		// 	},
+		// },
 		disableHostCheck: true,
 		allowedHosts: ["http://www.kyonggiupass.com:8080/"],
 	},
