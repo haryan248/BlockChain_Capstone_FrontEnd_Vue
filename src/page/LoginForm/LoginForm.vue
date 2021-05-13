c4aacf37f742c3d7bfbbddee7d6083243a71bc01
ryantutoring <ryan.r@mdcrew.co.kr>
2021-05-13 17:31:51 +0900

    오류 수정

--- src/page/LoginForm/LoginForm.vue
+++ src/page/LoginForm/LoginForm.vue
@@ -164,7 +164,7 @@ export default {
 		//did 발급
 		async getUserDID() {
 			try {
-				const response = await this.$axios.post("/api/generatedid/", { params: { key: localStorage.getItem("key"), SimplePassword: localStorage.getItem("simplePassword") } })
+				const response = await this.$axios.post("/api/generatedid/", {}, { params: { key: localStorage.getItem("key"), SimplePassword: localStorage.getItem("simplePassword") } })
 				if (response.status === 201) {
 					localStorage.setItem("did", response.data.did)
 					this.showSuccess("학생증 발급 완료", "학생증 발급이 완료되었습니다.")
