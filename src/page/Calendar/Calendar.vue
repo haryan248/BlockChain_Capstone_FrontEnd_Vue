d8c9b0f840736d7de1214da3511cade361743191
haryan248 <haryan96@naver.com>
2021-05-19 19:10:34 +0900

    캘린더 기능 추가

--- src/page/Calendar/Calendar.vue
+++ src/page/Calendar/Calendar.vue
@@ -3,17 +3,52 @@
 		<div class="container bg-gray" :class="{ 'bg-dark': darkModeState }">
 			<div class="calendar__content">
 				<Header :title="'캘린더'" @confirmSetting="confirmSetting" />
-				<!-- <Calendar /> -->
-				<v-calendar is-expanded :attributes="attributes" :is-dark="darkModeState">
-					<template #day-popover="{ day, format, masks }">
-						<div class="text-xs text-gray-300 font-semibold text-center">
-							{{ format(day.date, masks.dayPopover) }}
+				<v-calendar :attributes="attributes" :is-dark="darkModeState" @dayclick="onDayClick" is-expanded>
+					<!-- <template #day-popover="{ dayTitle, attributes }">
+						<div>
+							<div class="text-xs text-gray-300 font-semibold text-center">
+								{{ dayTitle }}
+							</div>
+							<ul>
+								<li v-for="attr in attributes" :key="attr.key">
+									{{ attr.customData.building }}
+								</li>
+							</ul>
 						</div>
-					</template>
+					</template> -->
 				</v-calendar>
-				<div class="schedule__content">
+				<div class="schedule__content" v-if="!loading">
 					<div class="item__content">
-						{{ 11 }}
+						<div v-for="(item, index) in selectDate" :key="index" class="entry__content">
+							<div class="schedule__date">
+								{{ item.day }}
+							</div>
+							<div class="entry-empty__list" v-if="item.attributes.length === 0">
+								<li class="list__item">
+									<div class="empty-list__item">
+										<div class="item__content">
+											<div class="item__summary">
+												<h3 class="class__room">방문한 기록이 없어요!</h3>
+											</div>
+										</div>
+									</div>
+								</li>
+							</div>
+							<div class="entry-list__content" v-else>
+								<div v-for="(attribute, index) in item.attributes" :key="index">
+									<li class="list__item">
+										<div class="entry-list__item">
+											<div class="item__content">
+												<div class="item__summary">
+													<h3 class="entry__time">{{ attribute.time }}</h3>
+													<h3 class="class__room">{{ attribute.building }}강의동 ( {{ checkBuildingName(attribute.building, index) }} )</h3>
+												</div>
+											</div>
+										</div>
+									</li>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -25,58 +60,124 @@
 export default {
 	name: "Calendar",
 	data() {
-		const todos = [
-			{
-				description: "Take Noah to basketball practice.",
-				isComplete: false,
-				dates: [new Date()], // Every Friday
-				color: "red",
-				key: "today",
-				highlight: true,
-			},
-		]
 		return {
-			attrs: [
+			curYear: "",
+			curMonth: "",
+			curDay: "",
+			buildingName: [],
+			loading: true,
+			selectDate: [
 				{
-					key: "today",
-					highlight: true,
-					dates: new Date(),
-				},
-				{
-					bar: "red",
-					dates: [
-						new Date(2021, 4, 4), // Jan 4th
-						new Date(2021, 4, 10), // Jan 10th
-						new Date(2021, 4, 15), // Jan 15th
-					],
+					day: "",
+					attributes: [],
 				},
 			],
-			incId: todos.length,
-			todos,
+			today: {
+				highlight: {
+					color: "blue",
+					fillMode: "light",
+				},
+				dates: new Date(),
+			},
+			attributes: [
+				{
+					dot: "",
+					key: 0,
+					customData: {
+						building: "",
+					},
+					dates: null,
+				},
+				{
+					highlight: {
+						color: "blue",
+						fillMode: "light",
+					},
+					dates: new Date(),
+				},
+			],
 			darkModeState: this.$shared.checkDarkMode(),
 		}
 	},
-	computed: {
-		attributes() {
-			return [
-				// Attributes for todos
-				...this.todos.map((todo) => ({
-					dates: todo.dates,
-					bar: {
-						color: todo.color,
-						class: todo.isComplete ? "opacity-75" : "",
-					},
-					popover: {
-						label: todo.description,
-					},
-					customData: todo,
-				})),
-			]
-		},
+	created() {
+		this.setDate()
+		this.getEntry()
+		this.selectDate[0].day = this.curYear + "-" + this.curMonth + "-" + this.curDay
 	},
 	methods: {
 		confirmSetting() {
 			this.darkModeState = this.$shared.checkDarkMode()
+		},
+		setDate() {
+			let date = new Date()
+			this.curYear = date.getFullYear()
+			this.curMonth = date.getMonth() + 1
+			this.curMonth = this.curMonth >= 10 ? this.curMonth : "0" + this.curMonth
+			this.curDay = date.getDate()
+			this.curDay = this.curDay >= 10 ? this.curDay : "0" + this.curDay
+		},
+		onDayClick(day) {
+			console.log(day)
+			this.selectDate[0].day = day.id
+			this.selectDate[0].attributes = []
+			day.attributes.forEach((item, i) => {
+				if (item.customData !== undefined) {
+					this.selectDate[0].attributes[i] = { building: item.customData.building, time: item.customData.time }
+				}
+			})
+		},
+		checkBuildingName(building, index) {
+			switch (Number(building)) {
+				case 1:
+					this.buildingName[index] = "진리관"
+					return this.buildingName[index]
+				case 2:
+					this.buildingName[index] = "성신관"
+					return this.buildingName[index]
+				case 3:
+					this.buildingName[index] = "애경관"
+					return this.buildingName[index]
+				case 4:
+					this.buildingName[index] = "예지관"
+					return this.buildingName[index]
+				case 5:
+					this.buildingName[index] = "덕문관"
+					return this.buildingName[index]
+				case 6:
+					this.buildingName[index] = "광교관"
+					return this.buildingName[index]
+				case 7:
+					this.buildingName[index] = "집현관"
+					return this.buildingName[index]
+				case 8:
+					this.buildingName[index] = "육영관"
+					return this.buildingName[index]
+				case 9:
+					this.buildingName[index] = "육영관"
+					return this.buildingName[index]
+			}
+		},
+		async getEntry() {
+			try {
+				const response = await this.$axios.get("/api/getentry/", {
+					params: { key: localStorage.getItem("key"), SimplePassword: localStorage.getItem("simplePassword"), did: localStorage.getItem("did"), year: this.curYear, month: this.curMonth, day: this.curDay },
+				})
+				if (response.status === 201) {
+					response.data.transaction.forEach((item, i) => {
+						this.attributes[i] = { key: i, bar: "red", customData: { building: item.building, time: item.time }, dates: new Date(item.date) }
+					})
+					this.attributes.push(this.today)
+					this.attributes = [...this.attributes]
+					this.loading = false
+					this.onDayClick(new Date())
+				}
+			} catch (error) {
+				if (error.response) {
+					if (error.response.data.msg === "DID를 찾을 수 없습니다.") {
+						console.log(error)
+					}
+				}
+			}
 		},
 	},
 }
