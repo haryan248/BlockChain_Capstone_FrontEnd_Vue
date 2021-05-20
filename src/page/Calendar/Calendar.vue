71bcf9265814459e2453976094f971a8309e5a71
haryan248 <haryan96@naver.com>
2021-05-20 15:07:23 +0900

    캘린더 출입여부 코드 완료

--- src/page/Calendar/Calendar.vue
+++ src/page/Calendar/Calendar.vue
@@ -3,20 +3,7 @@
 		<div class="container bg-gray" :class="{ 'bg-dark': darkModeState }">
 			<div class="calendar__content">
 				<Header :title="'캘린더'" @confirmSetting="confirmSetting" />
-				<v-calendar :attributes="attributes" :is-dark="darkModeState" @dayclick="onDayClick" @update:to-page="toPage" is-expanded>
-					<!-- <template #day-popover="{ dayTitle, attributes }">
-						<div>
-							<div class="text-xs text-gray-300 font-semibold text-center">
-								{{ dayTitle }}
-							</div>
-							<ul>
-								<li v-for="attr in attributes" :key="attr.key">
-									{{ attr.customData.building }}
-								</li>
-							</ul>
-						</div>
-					</template> -->
-				</v-calendar>
+				<v-date-picker ref="calendar" :attributes="attributes" :is-dark="darkModeState" v-model="selectDate.date" @dayclick="onDayClick" @update:to-page="toPage" is-expanded />
 				<div v-if="loading" class="loading__overlay">
 					<div class="loading__progressbar">
 						<h5 class="calendar_loading">출입 여부를 불러오는 중입니다.</h5>
@@ -28,10 +15,10 @@
 						<div v-for="(item, index) in selectDate" :key="index" class="entry__content">
 							<div class="schedule__date">
 								{{ item.day }}
-								<!-- <i class="pi pi-spin pi-spinner" style="fontSize: 2rem"></i> -->
 							</div>
 							<div class="refresh__button">
-								<Button icon="pi pi-refresh" @click="getEntry" class="border__none-refresh p-button-sm p-button-rounded p-button-secondary" />
+								<h4 class="load__text">{{ loadMonth }}월 출입 기록</h4>
+								<Button icon="pi pi-download" @click="getEntry" class="border__none-refresh p-button-sm p-button-rounded p-button-secondary" />
 							</div>
 							<div v-if="loading" class="entry-empty__list" :class="{ dark__mode: darkModeState }">
 								<li class="list__item">
@@ -85,6 +72,7 @@ export default {
 			curYear: "",
 			curMonth: "",
 			curDay: "",
+			loadMonth: "",
 			buildingName: [],
 			loading: false,
 			selectDate: [
@@ -96,7 +84,7 @@ export default {
 			today: {
 				highlight: {
 					color: "blue",
-					fillMode: "light",
+					fillMode: "outline",
 				},
 				dates: new Date(),
 			},
@@ -106,13 +94,15 @@ export default {
 					key: 0,
 					customData: {
 						building: "",
+						time: "",
 					},
+					compareDate: null,
 					dates: null,
 				},
 				{
 					highlight: {
 						color: "blue",
-						fillMode: "light",
+						fillMode: "outline",
 					},
 					dates: new Date(),
 				},
@@ -120,7 +110,7 @@ export default {
 			darkModeState: this.$shared.checkDarkMode(),
 		}
 	},
-	created() {
+	mounted() {
 		this.setDate()
 		this.selectDate[0].day = this.curYear + "-" + this.curMonth + "-" + this.curDay
 	},
@@ -128,21 +118,28 @@ export default {
 		confirmSetting() {
 			this.darkModeState = this.$shared.checkDarkMode()
 		},
+		//초기 데이트 설정
 		setDate() {
-			let date = new Date()
-			this.curYear = date.getFullYear()
-			this.curMonth = date.getMonth() + 1
+			this.curYear = this.today.dates.getFullYear()
+			this.curMonth = this.today.dates.getMonth() + 1
 			this.curMonth = this.curMonth >= 10 ? this.curMonth : "0" + this.curMonth
-			this.curDay = date.getDate()
+			this.curDay = this.today.dates.getDate()
 			this.curDay = this.curDay >= 10 ? this.curDay : "0" + this.curDay
 		},
+		//달력 넘길때
 		toPage(page) {
+			this.loadMonth = page.month
 			page.month = page.month >= 10 ? page.month : "0" + page.month
-			this.selectDate[0].day = page.year + "-" + page.month + "-01"
-			this.selectDate[0].attributes = []
+			this.curYear = page.year
+			this.curMonth = page.month
+			this.curDay = "01"
 		},
+		//날짜 클릭 시
 		onDayClick(day) {
-			console.log(day)
+			this.curMonth = day.month
+			this.curYear = day.year
+			this.curDay = day.day
+
 			this.selectDate[0].day = day.id
 			this.selectDate[0].attributes = []
 			day.attributes.forEach((item, i) => {
@@ -151,6 +148,7 @@ export default {
 				}
 			})
 		},
+		//강의동에 따른 건물 이름 매핑
 		checkBuildingName(building, index) {
 			switch (Number(building)) {
 				case 1:
@@ -185,18 +183,23 @@ export default {
 		async getEntry() {
 			try {
 				this.loading = true
-
 				const response = await this.$axios.get("/api/getentry/", {
 					params: { key: localStorage.getItem("key"), SimplePassword: localStorage.getItem("simplePassword"), did: localStorage.getItem("did"), year: this.curYear, month: this.curMonth, day: this.curDay },
 				})
 				if (response.status === 201) {
 					response.data.transaction.forEach((item, i) => {
-						this.attributes[i] = { key: i, bar: "red", customData: { building: item.building, time: item.time }, dates: new Date(item.date) }
+						this.attributes[i] = { key: i, bar: "red", customData: { building: item.building, time: item.time }, compareDate: item.date, dates: new Date(item.date) }
 					})
 					this.attributes.push(this.today)
 					this.attributes = [...this.attributes]
+					this.selectDate[0].day = this.curYear + "-" + this.curMonth + "-" + this.curDay
 					this.loading = false
-					this.onDayClick(new Date())
+					let j = 0
+					this.attributes.forEach((item) => {
+						if (this.selectDate[0].day === item.compareDate) {
+							this.selectDate[0].attributes[j++] = { building: item.customData.building, time: item.customData.time }
+						}
+					})
 				}
 			} catch (error) {
 				if (error.response) {
@@ -211,4 +214,13 @@ export default {
 </script>
 <style scoped>
 @import "./calendar.css";
+</style>
+<style>
+.vc-day-content:focus {
+	font-weight: var(--font-bold);
+	background-color: transparent !important;
+}
+.vc-day-content:hover {
+	background-color: transparent !important;
+}
 </style>
