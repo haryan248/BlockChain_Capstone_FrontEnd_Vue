257520c63bd973dec1d7d38e2109ee99367c41e2
haryan248 <haryan96@naver.com>
2021-05-24 16:39:01 +0900

    음식 사진 추가 및 css 수정

--- src/page/Order/Order.vue
+++ src/page/Order/Order.vue
@@ -5,22 +5,25 @@
 			<div>
 				<div class="order-content">
 					<HeaderSection :title="'주문서'" :subtitle="'음식을 주문해보세요.'" :darkModeState="darkModeState" />
-					<div class="menu__tab">
+					<div class="menu__tab" :class="{ dark__mode: darkModeState }">
+						<!-- Select university cafeteria -->
 						<div class="tab__list">
-							<MultiSelect v-model="selectedRestaurant" :options="restaurant" optionLabel="brand" placeholder="교내식당을 선택하세요." />
+							<MultiSelect class="order__multiselect" v-model="selectedRestaurant" :options="restaurant" optionLabel="brand" placeholder="교내식당을 선택하세요." />
 						</div>
+						<!-- Order QR code  -->
 						<div class="tab__list">
-							<Button label="주문서 확인" class="p-button-outlined p-button-secondary" @click="openModal" />
+							<Button label="주문서 확인" class="order__qr-button p-button-outlined p-button-secondary" @click="openOrderQRModal" />
 						</div>
 					</div>
-					<OrderList v-model="products" listStyle="height:auto" dataKey="id">
+					<!-- OrderList -->
+					<OrderList :class="{ dark__mode: darkModeState }" v-model="foods" listStyle="height:auto" dataKey="id">
 						<template #header>
 							식당 메뉴
 						</template>
 						<template #item="slotProps">
 							<div class="product-item">
 								<div class="image-container">
-									<img src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" :alt="slotProps.item.name" />
+									<img class="food__image" :src="slotProps.item.image" :alt="slotProps.item.name" />
 								</div>
 								<div class="product-list-detail">
 									<h6 class="p-mb-2">{{ slotProps.item.name }}</h6>
@@ -28,25 +31,22 @@
 									<span class="product-category">{{ slotProps.item.category }}</span>
 								</div>
 								<div class="product-list-action">
-									<h6 class="p-mb-2">${{ slotProps.item.price }}</h6>
-									<span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatus }}</span>
+									<h6 class="p-mb-2">{{ slotProps.item.price }} KRW</h6>
+									<span :class="'product-badge status-' + slotProps.item.inventoryStatus.toLowerCase()">{{ slotProps.item.inventoryStatusText }}</span>
 								</div>
 							</div>
 						</template>
 					</OrderList>
+					<!-- Order button -->
 					<div class="order__button">
 						<Button label="주문하기" icon="pi pi-check" />
 					</div>
 				</div>
 			</div>
 		</div>
-
-		<Dialog class="OrderModal" header="Header" :showHeader="false" v-model:visible="displayModal" :style="{ width: '80vw' }" :modal="true">
-			<!-- <template #content> -->
-			<QRVerification @goBack="closeModal" :did="DID" :SimplePassword="SimplePassword" />
-			<!-- <Button label="No" icon="pi pi-times" @click="closeResponsive" class="p-button-text" />
-								<Button label="Yes" icon="pi pi-check" @click="closeResponsive" autofocus /> -->
-			<!-- </template> -->
+		<!-- Order QR Modal -->
+		<Dialog class="OrderModal" :class="{ dark__mode: darkModeState }" header="Header" :showHeader="false" v-model:visible="displayOrderQRModal" :style="{ width: '80vw' }" :modal="true">
+			<QRVerification @goBack="closeOrderQRModal" :did="DID" :SimplePassword="SimplePassword" />
 		</Dialog>
 	</div>
 	<BottomNav :darkModeState="darkModeState" />
@@ -59,26 +59,69 @@ export default {
 	components: { QRVerification, HeaderSection },
 	data() {
 		return {
-			displayModal: false,
+			displayOrderQRModal: false,
 			selectedRestaurant: null,
 			restaurant: [
 				{ brand: "E-square", value: "E-square" },
-				{ brand: "감성코어", value: "Emotional core" },
+				{ brand: "감성코어", value: "Emotional Core" },
 			],
 			darkModeState: this.$shared.checkDarkMode(),
 			DID: localStorage.getItem("did"),
 			SimplePassword: localStorage.getItem("simplePassword"),
-			products: [
-				{ id: "1000", code: "f230fh0g3", name: "Bamboo Watch", description: "Product Description", image: "bamboo-watch.jpg", price: 65, category: "Accessories", quantity: 24, inventoryStatus: "INSTOCK", rating: 5 },
-				{ id: "1001", code: "nvklal433", name: "Black Watch", description: "Product Description", image: "black-watch.jpg", price: 72, category: "Accessories", quantity: 61, inventoryStatus: "INSTOCK", rating: 4 },
-				{ id: "1002", code: "zz21cz3c1", name: "Blue Band", description: "Product Description", image: "blue-band.jpg", price: 79, category: "Fitness", quantity: 2, inventoryStatus: "LOWSTOCK", rating: 3 },
-				{ id: "1003", code: "244wgerg2", name: "Blue T-Shirt", description: "Product Description", image: "blue-t-shirt.jpg", price: 29, category: "Clothing", quantity: 25, inventoryStatus: "INSTOCK", rating: 5 },
-				{ id: "1004", code: "h456wer53", name: "Bracelet", description: "Product Description", image: "bracelet.jpg", price: 15, category: "Accessories", quantity: 73, inventoryStatus: "INSTOCK", rating: 4 },
-				{ id: "1005", code: "av2231fwg", name: "Brown Purse", description: "Product Description", image: "brown-purse.jpg", price: 120, category: "Accessories", quantity: 0, inventoryStatus: "OUTOFSTOCK", rating: 4 },
-				{ id: "1006", code: "bib36pfvm", name: "Chakra Bracelet", description: "Product Description", image: "chakra-bracelet.jpg", price: 32, category: "Accessories", quantity: 5, inventoryStatus: "LOWSTOCK", rating: 3 },
-				{ id: "1007", code: "mbvjkgip5", name: "Galaxy Earrings", description: "Product Description", image: "galaxy-earrings.jpg", price: 34, category: "Accessories", quantity: 23, inventoryStatus: "INSTOCK", rating: 5 },
-				{ id: "1008", code: "vbb124btr", name: "Game Controller", description: "Product Description", image: "game-controller.jpg", price: 99, category: "Electronics", quantity: 2, inventoryStatus: "LOWSTOCK", rating: 4 },
-				{ id: "1009", code: "cm230f032", name: "Gaming Set", description: "Product Description", image: "gaming-set.jpg", price: 299, category: "Electronics", quantity: 63, inventoryStatus: "INSTOCK", rating: 3 },
+			// 임시 음식 데이터
+			foods: [
+				{
+					id: "1000",
+					code: "f230fh0g3",
+					name: "돈까스",
+					description: "Product Description",
+					image: require("../../assets/Food/돈까스.jpg"),
+					price: "6,000",
+					category: "일식",
+					quantity: 24,
+					inventoryStatusText: "주문 가능",
+					inventoryStatus: "INSTOCK",
+					rating: 5,
+				},
+				{
+					id: "1001",
+					code: "nvklal433",
+					name: "라면",
+					description: "Product Description",
+					image: require("../../assets/Food/라면.jpg"),
+					price: "4,000",
+					category: "분식",
+					quantity: 61,
+					inventoryStatusText: "주문 가능",
+					inventoryStatus: "INSTOCK",
+					rating: 4,
+				},
+				{
+					id: "1002",
+					code: "zz21cz3c1",
+					name: "떡볶이",
+					description: "Product Description",
+					image: require("../../assets/Food/떡볶이.jpg"),
+					price: "3,000",
+					category: "분식",
+					quantity: 2,
+					inventoryStatusText: "재료 부족",
+					inventoryStatus: "LOWSTOCK",
+					rating: 3,
+				},
+				{
+					id: "1003",
+					code: "244wgerg2",
+					name: "김밥",
+					description: "Product Description",
+					image: require("../../assets/Food/김밥.jpg"),
+					price: "2,000",
+					category: "분식",
+					quantity: 25,
+					inventoryStatusText: "주문 가능",
+					inventoryStatus: "INSTOCK",
+					rating: 5,
+				},
 			],
 		}
 	},
@@ -86,14 +129,16 @@ export default {
 		this.$shared.checkGoogleLogin(this.$gAuth)
 	},
 	methods: {
+		// 다크모드 설정
 		confirmSetting() {
 			this.darkModeState = this.$shared.checkDarkMode()
 		},
-		openModal() {
-			this.displayModal = true
+		// 주문 qr코드 토글 함수
+		openOrderQRModal() {
+			this.displayOrderQRModal = true
 		},
-		closeModal() {
-			this.displayModal = false
+		closeOrderQRModal() {
+			this.displayOrderQRModal = false
 		},
 	},
 }
@@ -110,5 +155,68 @@ export default {
 }
 .p-orderlist .p-orderlist-list {
 	height: 60% !important;
+}
+/* 스크롤바 custom */
+.p-orderlist-list::-webkit-scrollbar {
+	width: 7px;
+}
+.p-orderlist-list::-webkit-scrollbar-thumb {
+	background-color: #607d8b;
+	border-radius: 5px;
+	background-clip: padding-box;
+	border: 2px solid transparent;
+}
+.p-orderlist-list::-webkit-scrollbar-track {
+	background-color: #cbd5e0;
+	border-radius: 3px;
+	box-shadow: inset 0px 0px 5px white;
+}
+button.p-button.p-component.order__qr-button.p-button-outlined.p-button-secondary {
+	background: #ffffff;
+	border: 1px solid #cbd5e0;
+}
+/* 다크모드 css */
+.p-orderlist.p-component.dark__mode .p-orderlist-header {
+	background: #333536;
+	color: #ffffff;
+	border: 1px solid #333536;
+}
+.p-orderlist.p-component.dark__mode .p-orderlist-list {
+	border: 1px solid #333536;
+	background: #333536;
+	color: #ffffff;
+}
+.p-orderlist.p-component.dark__mode .p-orderlist-list:focus {
+	box-shadow: inset 0 0 0 0.15rem #607d8b;
+}
+.p-orderlist.p-component.dark__mode .p-orderlist-list .p-orderlist-item {
+	color: #ffffff;
+}
+.p-orderlist.p-component.dark__mode .p-orderlist-item.p-highlight {
+	background: #607d8b;
+}
+.menu__tab.dark__mode .order__qr-button {
+	background-color: #333536;
+	color: #ffffff;
+	border: 1px solid #333536;
+}
+.menu__tab.dark__mode .order__multiselect,
+.menu__tab.dark__mode .p-multiselect-header {
+	background-color: #333536;
+	border: 1px solid #333536;
+}
+.p-multiselect:not(.p-disabled):hover {
+	box-shadow: none;
+	border-color: #607d8b;
+}
+.menu__tab.dark__mode .p-multiselect-label.p-placeholder {
+	color: #ffffff;
+}
+.menu__tab.dark__mode button.p-button.p-component.order__qr-button.p-button-outlined.p-button-secondary {
+	background: #333536;
+	border: 1px solid #333536;
+}
+.p-dialog.p-component.OrderModal.dark__mode .p-dialog-content {
+	background: #333536;
 }
 </style>
--- src/tool/shared.ts
+++ src/tool/shared.ts
@@ -1,15 +1,19 @@
+// 공통 함수
 const shared = {
+	// 로그인 유효성 검사 함수
 	checkGoogleLogin(gAuth) {
 		setTimeout(() => {
 			// console.log(gAuth.instance.currentUser.get().isSignedIn())
 			if (gAuth.instance.currentUser.get().isSignedIn() === false) localStorage.removeItem("key")
 		}, 2000)
 	},
+	// 다크모드 체크 함수
 	checkDarkMode() {
 		if (JSON.parse(localStorage.getItem("DarkMode")) == true) {
 			return true
 		} else return false
 	},
+	// 관리자 모드 체크 함수
 	checkAdminMode() {
 		if (JSON.parse(localStorage.getItem("AdminMode")) == true) {
 			return true
