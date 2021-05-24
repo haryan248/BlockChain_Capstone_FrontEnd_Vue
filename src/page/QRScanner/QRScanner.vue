4673c6a9c7098306048202ecb66f073a98253571
haryan248 <haryan96@naver.com>
2021-05-24 19:32:18 +0900

    qr 인증 css 수정

--- src/page/QRScanner/QRScanner.vue
+++ src/page/QRScanner/QRScanner.vue
@@ -6,29 +6,37 @@
 				<div class="qr-content">
 					<div class="center stream">
 						<div class="close__button">
-							<Button icon="pi pi-times" @click="goBack" class="p-button-lg" alt="switch camera" />
+							<Button icon="pi pi-times" @click="goBack" class="p-button" alt="switch camera" />
 						</div>
-						<!-- Camera switch -->
-						<button class="conver__button" type="button" @click="switchCamera"></button>
-						<qr-stream :camera="camera" @decode="onDecode" class="mb" @init="onInit">
+						<qr-stream :camera="camera" @decode="onDecode" @init="onInit">
 							<!-- Loading screen -->
-							<div class="loading-indicator-qr" v-if="loading">
-								잠시만 기다려주세요.
+							<div class="loading-indicator-qr" v-if="loading && !firstLoading">
+								카메라가 로딩중입니다.
 								<ProgressBar mode="indeterminate" style="height: .5em" />
 							</div>
-							<div v-if="validationSuccess" class="validation-success">
-								{{ result }}
+							<div v-else-if="validationSuccess" class="validation-success">
+								<i class="pi pi-spin pi-spinner" style="fontSize: 5rem"></i>
 							</div>
-
-							<div v-if="validationFailure" class="validation-failure">
-								This is NOT a URL!
+							<div v-else-if="validationPending" class="validation-pending">
+								<i class="pi pi-spin pi-spinner" style="fontSize: 5rem"></i>
 							</div>
-
-							<div v-if="validationPending" class="validation-pending">
-								Long validation in progress...
+							<!-- center QR screen -->
+							<div v-else>
+								<div class="QR_title">QR 인증</div>
+								<div class="frame">
+									<div class="center__box"><i class="pi pi-plus" style="fontSize: 1rem;"></i></div>
+									<div class="left_top-box"></div>
+									<div class="right_top-box"></div>
+									<div class="left_bottom-box"></div>
+									<div class="right_bottom-box"></div>
+								</div>
+								<div class="QR_descirpt"><span class="focus">QR코드</span>로 인증을 진행할 수 있습니다. <br />카메라 화면 안에 <span class="focus">QR 코드</span>를 <span class="focus">인식</span>시켜주세요.</div>
 							</div>
-							<div v-else style="color: #ff4b4b;" class="frame"></div>
 						</qr-stream>
+						<!-- Camera switch -->
+						<div class="switch__button" @click="switchCamera">
+							<div>카메라 전환</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -42,7 +50,7 @@ export default {
 	name: "QRScanner",
 	components: { QrStream },
 	data() {
-		return { isValid: undefined, result: "", error: "", loading: false, camera: "front", showScanConfirmation: false }
+		return { isValid: undefined, result: "", error: "", loading: false, camera: "front", showScanConfirmation: false, firstLoading: false }
 	},
 	mounted() {
 		this.$shared.checkGoogleLogin(this.$gAuth)
@@ -63,17 +71,12 @@ export default {
 	methods: {
 		// qr 디코드
 		async onDecode(result) {
+			this.firstLoading = true
 			this.result = result
 			this.turnCameraOff()
-
 			// pretend it's taking really long
-			await this.timeout(3000)
 			this.generateEntry(result)
-
-			this.play("https://soundbible.com/mp3/Checkout Scanner Beep-SoundBible.com-593325210.mp3")
-
-			// some more delay, so users have time to read the message
-			await this.timeout(2000)
+			await this.timeout(3000)
 			this.turnCameraOn()
 		},
 		// 인증 사운드
@@ -99,7 +102,6 @@ export default {
 		},
 		// 카메라 전환
 		switchCamera() {
-			console.log(this.camera)
 			switch (this.camera) {
 				case "front":
 					this.camera = "rear"
@@ -109,12 +111,12 @@ export default {
 					break
 			}
 		},
-
 		timeout(ms) {
 			return new Promise((resolve) => {
 				window.setTimeout(resolve, ms)
 			})
 		},
+		// qr 인증시 블록체인 네트워크에 tx 발생
 		async generateEntry(result) {
 			let date = new Date()
 			let year = date.getFullYear()
@@ -155,10 +157,14 @@ export default {
 						this.showError("본인 인증 오류", "죄송합니다. \n본인 인증에 오류가 있습니다.")
 					} else if (error.response.data.msg === "timestamp error") {
 						this.showError("유효 시간 오류", "죄송합니다. \n유효 시간이 지났습니다. 다시 찍어주세요.")
+					} else {
+						this.showError("인증 오류", "죄송합니다. \n본인 인증에 오류가 있습니다.")
 					}
 				}
 			}
+			this.play("https://soundbible.com/mp3/Checkout Scanner Beep-SoundBible.com-593325210.mp3")
 		},
+		// 카메라 초기 세팅
 		async onInit(promise) {
 			this.loading = true
 			try {
